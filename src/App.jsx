15c06e484c5068c1f87e912523bf3abbfc725208
kitsunelynx <kitsunelynx2@gmail.com>
2025-07-28 19:44:22 +0530

    App.tsx fix

--- src/App.jsx
+++ src/App.jsx
@@ -6,19 +6,7 @@ import Product from './pages/Product.jsx'
 import Careers from './pages/Careers.jsx'
 import Blog from './pages/Blog.jsx'
 
-<<<<<<< HEAD
-const App = () => (
-  <Router>
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/about" element={<About />} />
-      <Route path="/product" element={<Product />} />
-      <Route path="/careers" element={<Careers />} />
-      <Route path="/blog" element={<Blog />} />
-      <Route path="/blog/:slug" element={<Blog />} />
-    </Routes>
-  </Router>
-=======
+
 const App = ({ initialData = {} }) => (
   <Routes>
     <Route path="/" element={<Home />} />
@@ -27,7 +15,7 @@ const App = ({ initialData = {} }) => (
     <Route path="/careers" element={<Careers initialJobs={initialData.jobs} />} />
     <Route path="/blog" element={<Blog />} />
   </Routes>
->>>>>>> 7e5e58a14ca11adee6b44b08065b55af133cd4ee
+
 )
 
 export default App
