8eaeab98f91839830b34f7dd681d95bfb3d8a0f2
Hari Krishna <kitsunelynx2@gmail.com>
2025-08-07 19:54:29 +0530

    refactor carousel styles

--- src/components/CardCarousel.jsx
+++ src/components/CardCarousel.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect, useRef, useCallback } from 'react';
+import '../styles/carousel.css';
 
 const CardCarousel = ({ items, cardComponent, className = '' }) => {
   const CardComponent = cardComponent;
